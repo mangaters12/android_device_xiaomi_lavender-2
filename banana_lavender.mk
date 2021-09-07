b30e77a2329f0b078bddd37bdb77b2a05e27bda1
AbrarNoob <hasinabrarhaque2000@gmail.com>
2021-09-07 18:36:31 +0600

    let's go official

--- banana_lavender.mk
+++ banana_lavender.mk
@@ -13,7 +13,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, vendor/banana/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_STOCK_ARCORE := true
-WITH_GMS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+BANANA_BUILDTYPE := OFFICIAL
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
